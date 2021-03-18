e2e79a29a44481f5761381345d5cc10c3ad27c9b
clink4 <clink4@nd.edu>
2021-03-17 20:51:23 -0400

    testing

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func main() {
 	})
 
   router.GET("/mark", func(c *gin.Context) {
-    c.String(http.StatusOK, string(blackfriday.Run([]byte("**hi!**"))))
+    c.String(http.StatusOK, string(blackfriday.Run([]byte("**hi! :)) **"))))
   })
 
   router.GET("/repeat", repeatHandler(repeat))
