73dce6202d43de1eca3d7108285e4e298e2cc472
clink4 <clink4@nd.edu>
2021-03-25 01:53:13 -0400

    finishing emails

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 
 	//"gopkg.in/mail.v2"
 	//"fmt"
@@ -21,28 +22,59 @@ import (
 )
 
 func sleeper(c *gin.Context) {
-	var available, to_display []string
-	for {
-		log.Print("hello my name is ", string(c.Query("month")))
-		log.Print("and the time is ", time.Now().String())
-		available, to_display = check_available(c)
-		log.Print(available, to_display)
-		time.Sleep(30 * time.Second)
+	email := c.DefaultQuery("email", "none")
+
+	var available, to_display, keys []string
+	available, to_display, keys = check_available(c)
+
+	var k_map map[string]string
+	k_map = make(map[string]string)
+
+	for _, s := range keys {
+		k_map[s] = "true"
 	}
 
-	//send an email
-	//from := "gymspotsND@gmail.com"
-	//pass := "<a1b2c3d4!>"
+	found := 0
 
+	for {
+		//log.Print("hello my name is ", string(c.Query("month")))
+		//log.Print("and the time is ", time.Now().String())
+		time.Sleep(30 * time.Second)
+		available, to_display, _ = check_available(c) //NOTE: NEED to use ORIGINAL keys here
+
+		for i, s := range to_display {
+			//Friday, March 26, 2021: 1:30 PM - 2:30 PM 8 spot(s)
+			for _, k := range keys {
+				if strings.Contains(s, k) && k_map[keys[i]] == "true" && available[i] != "No Spots Available"{
+					k_map[keys[i]] = "false"
+					found++
+					log.Print(s)
+					if email != "none" {
+						send_email(string(email), s)
+					}
+				}
+			}
+		}
+
+		//log.Print(available, to_display, keys)
+
+		if found == len(available) {
+			break;
+		}
+	}
+
+
+}
+
+func send_email(to_email string, avail string) {
 	// Choose auth method and set it up
 	auth := smtp.PlainAuth("", "gymspotsnd@gmail.com", "a1b2c3d4!", "smtp.gmail.com")
 
 	// Here we do it all: connect to our server, set up a message and send it
-	to := []string{"clink4@nd.edu"}
-	msg := []byte("To: clink4@nd.edu\r\n" +
-		"Subject: Why are you not using Mailtrap yet?\r\n" +
-		"\r\n" +
-		"Here’s the space for our great sales pitch\r\n")
+	to := []string{to_email}
+	msg := []byte("To: " + to_email + "\r\n" +
+		"Subject: Your Spot is Available\r\n" +
+		"\r\n" + avail + "\r\n")
 	err := smtp.SendMail("smtp.gmail.com:587", auth, "gymspotsnd@gmail.com", to, msg)
 	log.Print(err)
 
@@ -50,10 +82,9 @@ func sleeper(c *gin.Context) {
 		log.Print(err)
 		log.Fatal(err)
 	}
-
 }
 
-func check_available(c *gin.Context) ([]string, []string) {
+func check_available(c *gin.Context) ([]string, []string, []string) {
 
 	// Handle query string parameters
 	month := c.Query("month")
@@ -119,11 +150,12 @@ func check_available(c *gin.Context) ([]string, []string) {
 
 	m_int, _ := strconv.Atoi(mo)
 
-	all_times := []string{"5:45 AM", "6:00 AM", "6:15 AM", "6:45 AM", "7:15 AM", "7:30 AM", "7:45 AM", "8:30 AM", "8:45 AM",
-		"9:00 AM", "10:00 AM", "11:30 AM", "11:45 AM", "12:00 PM", "12:30 PM", "12:45 PM", "1:00 PM", "1:15 PM", "1:30 PM",
-		"1:45 PM", "2:00 PM", "2:15 PM", "2:30 PM", "2:45 PM", "3:00 PM", "4:30 PM", "4:45 PM", "5:00 PM", "5:30 PM", "5:45 PM",
-		"6:00 PM", "6:15 PM", "6:30 PM", "6:45 PM", "7:15 PM", "7:30 PM", "7:45 PM", "8:00 PM", "8:15 PM", "8:30 PM", "8:45 PM",
-		"9:00 PM", "9:15 PM", "9:30 PM", "9:45 PM", "10:00 PM", "10:15 PM", "10:30 PM", "10:45 PM", "11:00 PM"}
+	all_times := []string{"5:45 AM", "6:00 AM", "6:15 AM", "6:45 AM", "7:00 AM", "7:15 AM", "7:30 AM", "7:45 AM", "8:30 AM", "8:45 AM",
+		"9:00 AM", "9:15 AM", "9:30 AM", "10:00 AM", "10:15 AM", "10:30 AM", "10:45 AM", "11:00 AM", "11:15 AM", "11:30 AM", "11:45 AM",
+		"12:00 PM", "12:15 PM", "12:30 PM", "12:45 PM", "1:00 PM", "1:15 PM", "1:30 PM", "1:45 PM", "2:00 PM", "2:15 PM", "2:30 PM", "2:45 PM",
+		"3:00 PM", "4:30 PM", "4:45 PM", "5:00 PM", "5:30 PM", "5:45 PM", "6:00 PM", "6:15 PM", "6:30 PM", "6:45 PM", "7:15 PM", "7:30 PM",
+		"7:45 PM", "8:00 PM", "8:15 PM", "8:30 PM", "8:45 PM", "9:00 PM", "9:15 PM", "9:30 PM", "9:45 PM", "10:00 PM", "10:15 PM",
+		"10:30 PM", "10:45 PM", "11:00 PM"}
 
 	start_idx := 0
 	end_idx := 0
@@ -139,7 +171,8 @@ func check_available(c *gin.Context) ([]string, []string) {
 
 	var available []string
 	var to_display []string
-	// c.String(http.StatusOK, "availability : ")
+	var keys []string
+
 	for i := start_idx; i <= end_idx; i++ {
 		for j := i; j <= end_idx; j++ {
 			k_s := day_str + ", " + time.Month(m_int).String() + " " + string(day) + ", 2021: " + all_times[i] + " - " + all_times[j]
@@ -147,11 +180,12 @@ func check_available(c *gin.Context) ([]string, []string) {
 			if av != "" {
 				to_display = append(to_display, k_s+" "+av)
 				available = append(available, av)
+				keys = append(keys, k_s)
 			}
 		}
 	}
 
-	return available, to_display
+	return available, to_display, keys
 }
 
 func main() {
@@ -175,7 +209,7 @@ func main() {
 
 	router.GET("/get", func(c *gin.Context) {
 		//wg.Add(1)
-		var available, to_display = check_available(c)
+		var available, to_display, _ = check_available(c)
 		// Display availability in HTML
 		log.Print(available)
 		slot_str := ""
